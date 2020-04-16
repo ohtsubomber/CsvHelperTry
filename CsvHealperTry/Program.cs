dbceede94eca27152e27729f5b92f9ae6a39ae6c
ohtsubo_masakazu <ohtsubo-masakazu@edu.brain.kyutech.jp>
2020-04-16 12:23:21 +0900

    ファイルからの読み込み部分をジェネリックメソッド化

--- CsvHealperTry/Program.cs
+++ CsvHealperTry/Program.cs
@@ -57,8 +57,58 @@ namespace CsvHealperTry
                     }
                 }
             }
+            Console.WriteLine();
+
+
+            {
+                var records = Read<C>("TextFile1.txt", typeof(CMap), new int[] { 1 });
+                foreach (var record in records)
+                {
+                    Console.WriteLine($"{record.A.Item} {record.B.Item}");
+                }
+            }
 
             Console.ReadLine();
+        }
+
+        public static List<T> Read<T>(string file, Type classMapType, IEnumerable<int> removeHeaderIndexes = null)
+        {
+            if (!File.Exists(file))
+            {
+                throw new FileNotFoundException();
+            }
+            var str = string.Empty;
+            using (var sr = new StreamReader(file))
+            {
+                str = sr.ReadToEnd();
+            }
+
+            //不要ヘッダ部削除
+            if (removeHeaderIndexes != null)
+            {
+                removeHeaderIndexes = removeHeaderIndexes.OrderByDescending(a => a);
+                var lines = str.Split(new string[] { Environment.NewLine }, StringSplitOptions.None).ToList();
+                foreach (var index in removeHeaderIndexes)
+                {
+                    lines.RemoveAt(index);
+                }
+
+                str = lines.Aggregate((a, b) => a + Environment.NewLine + b);
+            }
+
+            //データ読み込み
+            using (var ms = new MemoryStream(Encoding.UTF8.GetBytes(str)))
+            {
+                using (var sr = new StreamReader(ms))
+                {
+                    using (var csvReader = new CsvReader(sr, CultureInfo.InvariantCulture))
+                    {
+                        csvReader.Configuration.RegisterClassMap(classMapType);
+                        var records = csvReader.GetRecords<T>().ToList();
+                        return records;
+                    }
+                }
+            }
         }
     }
 
